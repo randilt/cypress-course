f427a13fd40d41d8fbb63e365094b143fadc398f
RandilT <randiltharusha72@gmail.com>
2024-08-19 10:56:55 +0530

    chore: Refactor Accordion component to use MUI v5 syntax and add data-testid to Accordion items

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,6 @@
 describe('Fundamentals Test', () => {
-  it('Visit to the web app', () => {
-    cy.visit('http://localhost:3000/fundamentals')
+  it('Contains Correct Header Text', () => {
+    cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals-header"]').should(
       'have.text',
       'Testing Fundamentals'
