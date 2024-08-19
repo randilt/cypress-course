5d612d97738170529ab8107eb631fe24275c7359
RandilT <randiltharusha72@gmail.com>
2024-08-19 11:33:50 +0530

    chore: Refactor Accordion component to use MUI v5 syntax and add data-testid to Accordion items

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -7,10 +7,13 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function ItemsAccordion({ items }) {
   return (
-    <div style={{ maxWidth: "70vw", minWidth: "50vw" }}>
+    <div
+      data-test="accordion-wrapper"
+      style={{ maxWidth: "70vw", minWidth: "50vw" }}
+    >
       {items.map((item) => {
         return (
-          <Accordion data-testid={`accordion-item-${item.id}`} key={item.id}>
+          <Accordion data-test={`accordion-item-${item.id}`} key={item.id}>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               id="panel1a-header"
--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,9 +1,21 @@
-describe('Fundamentals Test', () => {
-  it('Contains Correct Header Text', () => {
+describe('Fundamentals test', () => {
+  beforeEach(() => {
     cy.visit('/fundamentals')
-    cy.get('[data-test="fundamentals-header"]').should(
-      'have.text',
-      'Testing Fundamentals'
+  })
+  it('Contains correct header text', () => {
+    cy.contains(/Testing Fundamentals/i).should('be.visible')
+  })
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      'not.be.visible'
+    )
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      'be.visible'
+    )
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      'not.be.visible'
     )
   })
 })
